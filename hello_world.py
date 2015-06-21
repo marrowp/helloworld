463e4f99c5ba6c62502b8125a3dd127676593fa1
marrowp <marrow.paul@gmail.com>
2015-06-21 12:56:33 +0200

    The classic hello world program
    A test of changed GitHub path on machine due to problems with other
    paths

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 21 12:51:17 2015
+
+@author: Paul
+"""
+
+def hello_world():
+    #this says hello_world
+    print('hello world')
+    #thats it
+
+hello_world()
